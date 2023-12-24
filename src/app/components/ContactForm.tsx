c975e460119ad14204dead0ff926c0be94f188e2
Dario Mory <dario@mory.dev>
2023-12-24 03:55:40 +0100

    Apply basic styling to form and feedback messages

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -6,48 +6,75 @@ export default function ContactForm() {
     const [email, setEmail] = useState('');
     const [subject, setSubject] = useState('');
     const [message, setMessage] = useState('');
+    const [statusMessage, setStatusMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+
 
     const handleSubmit = async (event: React.SyntheticEvent) => {
         event.preventDefault();
-        const target = event.target as typeof event.target & {
-            name: { value: string };
-            email: { value: string };
-            subject: { value: string };
-            message: { value: string };
-        };
 
         const formData = {
-            name: target.name.value,
-            email: target.email.value,
-            subject: target.subject.value,
-            message: target.message.value,
+            name: name,
+            email: email,
+            subject: subject,
+            message: message,
         };
 
 
-        fetch('/api/contact', {
+        try {
+        const response = await fetch('/api/contact', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(formData),
         });
+
+        const data = await response.json();
+        if (response.ok) {
+            setStatusMessage('Message sent successfully!');
+            setIsError(false);
+            // Optionally clear the form here
+        } else {
+            setStatusMessage(data.message || 'Failed to send message.');
+            setIsError(true);
+        }
+    } catch (error) {
+        setStatusMessage('An error occurred.');
+        setIsError(true);
+    }
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" required onChange={(e) => setName(e.target.value)} />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 max-w-md mx-auto">
+            <div className="flex flex-col">
+                <label htmlFor="name" className="mb-1 font-semibold">Name:</label>
+                <input type="text" id="name" name="name" required onChange={(e) => setName(e.target.value)} className="text-gray-900 border border-gray-300 p-2 rounded-md focus:border-blue-500 focus:outline-none" />
+            </div>
 
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} />
+            <div className="flex flex-col">
+                <label htmlFor="email" className="mb-1 font-semibold">Email:</label>
+                <input type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)} className="text-gray-900 border border-gray-300 p-2 rounded-md focus:border-blue-500 focus:outline-none" />
+            </div>
 
-            <label htmlFor="Subject">Subject:</label>
-            <input type="subject" id="subject" name="subject" required onChange={(e) => setSubject(e.target.value)} />
+            <div className="flex flex-col">
+                <label htmlFor="subject" className="mb-1 font-semibold">Subject:</label>
+                <input type="text" id="subject" name="subject" required onChange={(e) => setSubject(e.target.value)} className="text-gray-900 border border-gray-300 p-2 rounded-md focus:border-blue-500 focus:outline-none" />
+            </div>
 
-            <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" required onChange={(e) => setMessage(e.target.value)}></textarea>
+            <div className="flex flex-col">
+                <label htmlFor="message" className="mb-1 font-semibold">Message:</label>
+                <textarea id="message" name="message" required onChange={(e) => setMessage(e.target.value)} className="text-gray-900 border border-gray-300 p-2 rounded-md focus:border-blue-500 focus:outline-none" rows="4"></textarea>
+            </div>
 
-            <button type="submit">Send</button>
+            <button type="submit" className="bg-orange-700 text-white p-2 rounded-md hover:bg-orange-600 mt-2">Send</button>
+
+            {statusMessage && (
+                <div className={`mt-2 p-2 rounded-md text-center font-semibold ${isError ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600'}`}>
+                    {statusMessage}
+                </div>
+            )}
         </form>
+
     );
 }
